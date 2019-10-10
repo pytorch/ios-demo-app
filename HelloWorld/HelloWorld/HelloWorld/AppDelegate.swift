53b8300101a6dca22ca3d1f6555810ce4b01c2ba
Ozgur Sahin <ozgurapps@gmail.com>
2019-10-11 00:01:50 +0300

    Removed UIWindowSceneDelegate as it doesnt work on ios 12, and fixed AppDelegate

--- HelloWorld/HelloWorld/HelloWorld/AppDelegate.swift
+++ HelloWorld/HelloWorld/HelloWorld/AppDelegate.swift
@@ -2,8 +2,5 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
-    }
+   var window: UIWindow?
 }
