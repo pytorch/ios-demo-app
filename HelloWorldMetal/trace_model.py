a1768761aea446486fc83e219faa86566fbcd310
Hanton Yang <hantonyang@fb.com>
2021-08-18 14:18:11 -0700

    Add HelloWorldMetal demo

--- HelloWorldMetal/trace_model.py
+++
@@ -1,9 +0,0 @@
-import torch
-import torchvision
-from torch.utils.mobile_optimizer import optimize_for_mobile
-
-model = torchvision.models.mobilenet_v2(pretrained=True)
-scripted_model = torch.jit.script(model)
-optimized_model = optimize_for_mobile(scripted_model, backend='metal')
-print(torch.jit.export_opnames(optimized_model))
-optimized_model._save_for_lite_interpreter('HelloWorldMetal/HelloWorldMetal/model/model.pt')
